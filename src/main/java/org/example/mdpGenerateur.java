bc34195c3a6605fa3a4671923b39ff50b07b8df0
christe36 <eboulemeizan@icloud.com>
2026-06-26 12:35:37 +0000

    Ajout du choix des minuscules dans le generateur de mot de passe

--- src/main/java/org/example/mdpGenerateur.java
+++ src/main/java/org/example/mdpGenerateur.java
@@ -5,44 +5,55 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-    public class mdpGenerateur {
-        // Définition des types de caractères
-        private static final String LOWER = "abcdefghijklmnopqrstuvwxyz";
-        private static final String UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private static final String DIGITS = "0123456789";
-        private static final String SYMBOLS = "!@#$%^&*()-_=+[]{}";
-        private final SecureRandom random = new SecureRandom();
+public class mdpGenerateur {
+    private static final String LOWER   = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPER   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String DIGITS  = "0123456789";
+    private static final String SYMBOLS = "!@#$%^&*()-_=+[]{}";
+    private final SecureRandom random = new SecureRandom();
 
-         // Génère un mot de passe robuste selon les paramètres choisis.
+    /**
+     * Génère un mot de passe selon les 4 types de caractères demandés par l'utilisateur.
+     * Au moins un caractère de chaque type activé est garanti dans le résultat final.
+     */
+    public String generate(int length, boolean includeUpper, boolean includeLower,
+                           boolean includeDigits, boolean includeSymbols) {
+        StringBuilder pool = new StringBuilder();
+        List<Character> passwordChars = new ArrayList<>();
 
-        public String generate(int length, boolean includeUpper, boolean includeDigits, boolean includeSymbols) {
-            StringBuilder characterPool = new StringBuilder(LOWER);
-            List<Character> passwordChars = new ArrayList<>();
-            // On s'assure d'avoir au moins un caractère de chaque type demandé
-            if (includeUpper) {
-                characterPool.append(UPPER);
-                passwordChars.add(UPPER.charAt(random.nextInt(UPPER.length())));
-            }
-            if (includeDigits) {
-                characterPool.append(DIGITS);
-                passwordChars.add(DIGITS.charAt(random.nextInt(DIGITS.length())));
-            }
-            if (includeSymbols) {
-                characterPool.append(SYMBOLS);
-                passwordChars.add(SYMBOLS.charAt(random.nextInt(SYMBOLS.length())));
-            }
-
-            // On complète le reste de la longueur demandée
-            for (int i = passwordChars.size(); i < length; i++) {
-                passwordChars.add(characterPool.charAt(random.nextInt(characterPool.length())));
-            }
-            //Le mélange pour que les premiers caractères ne soient pas prévisibles
-            Collections.shuffle(passwordChars);
-            StringBuilder finalPassword = new StringBuilder();
-            for (char c : passwordChars) {
-                finalPassword.append(c);
-            }
-            return finalPassword.toString();
+        // On construit le pool et on garantit au moins un caractère par type activé
+        if (includeUpper) {
+            pool.append(UPPER);
+            passwordChars.add(UPPER.charAt(random.nextInt(UPPER.length())));
         }
-    }
+        if (includeLower) {
+            pool.append(LOWER);
+            passwordChars.add(LOWER.charAt(random.nextInt(LOWER.length())));
+        }
+        if (includeDigits) {
+            pool.append(DIGITS);
+            passwordChars.add(DIGITS.charAt(random.nextInt(DIGITS.length())));
+        }
+        if (includeSymbols) {
+            pool.append(SYMBOLS);
+            passwordChars.add(SYMBOLS.charAt(random.nextInt(SYMBOLS.length())));
+        }
 
+        // Si aucun type sélectionné, on utilise les minuscules par défaut
+        if (pool.isEmpty()) {
+            pool.append(LOWER);
+        }
+
+        // On complète jusqu'à la longueur demandée
+        for (int i = passwordChars.size(); i < length; i++) {
+            passwordChars.add(pool.charAt(random.nextInt(pool.length())));
+        }
+
+        // Mélange pour éviter que les caractères obligatoires soient toujours en début
+        Collections.shuffle(passwordChars);
+
+        StringBuilder finalPassword = new StringBuilder();
+        for (char c : passwordChars) finalPassword.append(c);
+        return finalPassword.toString();
+    }
+}
